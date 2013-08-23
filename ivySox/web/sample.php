76999b2b54f30dd7e93c27efaa0d2612de210704
T3am5hark <infinite.jeff@gmail.com>
2013-08-23 15:48:49 -0700

    Cosmetics for mobile browsers...

--- ivySox/web/sample.php
+++ ivySox/web/sample.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <HTML>
+
+<head>
+    <title>Raspberry Pi!!!</title>
+    <meta name="viewport" content="width=device-width initial-scale=1 " />
+</head>
+
 <p>
 <h1>
 <?php echo "Here is some PHP!\n" ?>
