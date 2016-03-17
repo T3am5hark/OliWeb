e36aaaab99cf63bed897e8b2718417c5a7fe8e85
T3am5hark <infinite.jeff@gmail.com>
2016-03-17 07:21:04 +0000

    Added scripts to auto-run on boot, and updated install script to write to init.d location.

--- ivySox/install.sh
+++ ivySox/install.sh
@@ -10,9 +10,19 @@ then
   echo "Config file already exists, leaving local copy."
 else
   echo "Copying config file to $TARGET_PATH"
+  echo "NOTE: you will need to edit OliWebConfig.xml to point to absolute paths"
+  echo "for util, web, and cgi-bin folder locations."
   cp OliWebConfig.xml $TARGET_PATH
 fi
 
 # Copy startup script to init.d
-
-
+if [ -e /etc/init.d/oliweb ]
+then
+  echo "Init script already in place."
+else
+  echo "Copying autorun script to etc/init.d/oliweb"
+  cp oliweb.init.d /etc/init.d/oliweb
+  # Tell system about it
+  echo "Activating auto-start"
+  sudo update-rc.d oliweb defaults
+fi
